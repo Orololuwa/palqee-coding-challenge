fa1dd87c097ed86e168a8f40aa9f55fca1b1b8de
unknown <ea@afarapartners.com>
2022-03-01 00:30:21 +0100

    random quote description

--- pages/starwars/characters/index.js
+++ pages/starwars/characters/index.js
@@ -3,7 +3,7 @@ import StarWars from "..";
 const Characters = () => {
   return (
     <StarWars>
-      <h5>Characters</h5>
+        ...starwars
     </StarWars>
   );
 };
--- pages/starwars/index.js
+++ pages/starwars/index.js
@@ -1,9 +1,20 @@
 import BreadCrumbs from "containers/breadcrumbs";
+import { useEffect, useState } from "react";
 
 const StarWars = (props) => {
+  const [quote, setQuote] = useState("");
+
+  useEffect(() => {
+    fetch("http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote")
+      .then((response) => response.json())
+      .then((data) => setQuote(data.content));
+  }, []);
+
   return (
     <div>
       <BreadCrumbs />
+      <h5>Characters</h5>
+      <p>{quote}</p>
       <div>{props.children}</div>
     </div>
   );
