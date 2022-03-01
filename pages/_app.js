13c34b68a10ca81739c7aef1324922e03e91ce8e
unknown <ea@afarapartners.com>
2022-03-01 10:18:09 +0100

    apollo and graphQL setup

--- pages/_app.js
+++ pages/_app.js
@@ -5,18 +5,31 @@ import { StoreContextProvider } from "context/storeContext";
 import Header from "layout/header";
 import SideBar from "layout/sidebar";
 import "styles/globals.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: "http://localhost:57541" }),
+  cache: new InMemoryCache(),
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <StoreContextProvider>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <SideBar />
-        <div className="mainContent">
-          <Header />
-          <Component {...pageProps} />
-        </div>
-      </ThemeProvider>
-    </StoreContextProvider>
+    <ApolloProvider client={client}>
+      <StoreContextProvider>
+        <GlobalStyle />
+        <ThemeProvider theme={theme}>
+          <SideBar />
+          <div className="mainContent">
+            <Header />
+            <Component {...pageProps} />
+          </div>
+        </ThemeProvider>
+      </StoreContextProvider>{" "}
+    </ApolloProvider>
   );
 }
--- pages/starwars/characters/index.js
+++ pages/starwars/characters/index.js
@@ -1,7 +1,29 @@
 import { useEffect, useState } from "react";
 import StarWars from "..";
+import { gql, useQuery } from "@apollo/client";
+
+const ALL_CHARACTERS = gql`
+  {
+    allPeople {
+      people {
+        name
+        hairColor
+        skinColor
+        eyeColor
+        gender
+        homeworld {
+          name
+        }
+      }
+    }
+  }
+`;
 
 const Characters = () => {
+  const { loading, error, data } = useQuery(ALL_CHARACTERS);
+
+  console.log(data);
+
   const [quote, setQuote] = useState("");
 
   useEffect(() => {
@@ -9,6 +31,7 @@ const Characters = () => {
       .then((response) => response.json())
       .then((data) => setQuote(data.content));
   }, []);
+
   return (
     <StarWars>
       <h5>Characters</h5>
