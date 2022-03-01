17b5fb62390d0865bdfa33551b6c923e83e17686
unknown <ea@afarapartners.com>
2022-03-01 15:28:46 +0100

    max items per page adjustatble to 10 or 15

--- pages/starwars/characters/dataTable.js
+++ pages/starwars/characters/dataTable.js
@@ -1,6 +1,7 @@
 import Table from "react-data-table-component";
 import Pagination from "components/Pagination";
 import theme from "theme";
+import { useMemo, useState } from "react";
 
 //Table data
 const customStyles = {
@@ -89,21 +90,59 @@ const columns = [
 ];
 
 const DataTable = ({ data }) => {
+  const [pageSize, setPageSize] = useState(5);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  //Page change function
+  const currentTableData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * pageSize;
+    const lastPageIndex = firstPageIndex + pageSize;
+    return data.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage, pageSize]);
+
+  //Pagesize change function
+  const pageSizeHandler = (e) => {
+    const { value } = e.target;
+
+    setPageSize(value);
+  };
+
   return (
     <>
       <Table
         columns={columns}
-        data={data}
+        data={currentTableData}
         responsive
         customStyles={customStyles}
       />
-      <Pagination
-        className="pagination-bar"
-        currentPage={1}
-        totalCount={data.length}
-        pageSize={5}
-        onPageChange={(page) => onPageChange(page)}
-      />
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <form>
+          <label
+            htmlFor="pagesize"
+            style={{ fontSize: "14px", marginRight: "5px" }}
+          >
+            Select Page Size
+          </label>
+          <select id="pagesize" value={pageSize} onChange={pageSizeHandler}>
+            <option value={5}>5</option>
+            <option value={10}> 10</option>
+            <option value={15}>15</option>
+          </select>
+        </form>
+        <Pagination
+          className="pagination-bar"
+          currentPage={currentPage}
+          totalCount={data.length}
+          pageSize={pageSize}
+          onPageChange={(page) => setCurrentPage(page)}
+        />
+      </div>
     </>
   );
 };
